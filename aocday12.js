f7cf82b679d8f35b83c4cb0e094538e5819f94d9
Sara Culhane <sjcully@mac.com>
2020-12-13 13:53:38 -0500

    day12 part 1

--- aocday12.js
+++ aocday12.js
@@ -1,27 +1,77 @@
 const fs = require('fs');
-const filename = 'day11data.txt';
+const filename = 'day12data.txt';
 const moves = fs.readFileSync(filename).toString().split('\n')
 
-const shipLocation = (moves) => {
-    const dir = {
-        'E': 1,
-        'W': -1,
-        'S': -1,
-        'N' : 1
+const dir = {
+    'R90' : {
+        'W' : 'N',
+        'N': 'E',
+        'E': 'S',
+        'S' : 'W'
 
+
+    },
+    'L90' : {
+        'N' : 'W',
+        'E': 'N',
+        'S': 'E',
+        'W' : 'S'
+
+
+    },
+    '180': {
+        'N': 'S',
+        'E': 'W',
+        'S' : 'N',
+        'W' : 'E'
     }
-    let curDirection = 'E'
+}
+
+// problem 1
+
+const shipLocation = (dir, moves) => {
+
+    let curDir = 'E'
     let eastWest = 0
     let northSouth = 0
 
 
     for(let i = 0; i < moves.length; i++) {
-        let moves = moves[i]
+        
+        let move = moves[i]
+        
         if(move[0] === 'E') eastWest+= Number(move.slice(1, move.length))
         if(move[0] === 'W') eastWest-= Number(move.slice(1, move.length))
-        if(move[0] === 'N') nortSouth+= Number(move.slice(1, move.length))
+        if(move[0] === 'N') northSouth+= Number(move.slice(1, move.length))
         if(move[0] === 'S') northSouth-= Number(move.slice(1, move.length))
 
-        if(move[0] === )
-    }
+        if(move === 'R180' || move === 'L180') {
+            curDir = dir['180'][curDir]
+     }
+
+        if(move === 'R90' || move === 'L270') {
+            curDir = dir['R90'][curDir]
+            
+        }
+
+        if(move === 'L90' || move === 'R270') {
+            curDir = dir['L90'][curDir]
+        }
+
+        if(move[0] === 'F') {
+
+            if(curDir === 'E') eastWest+= Number(move.slice(1, move.length))
+            if(curDir === 'W') eastWest-= Number(move.slice(1, move.length))
+            if(curDir === 'N') northSouth+= Number(move.slice(1, move.length))
+            if(curDir === 'S') northSouth-= Number(move.slice(1, move.length))
+        }
+
 }
+    
+    return Math.abs(northSouth) + Math.abs(eastWest)
+
+}
+
+console.log(moves.length)
+
+console.log(shipLocation(dir, moves))
